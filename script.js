6ee44f12c63d8fafe9fccb9187228b51525cb795
gottacatchmall <gottacatchmall@gmail.com>
2016-11-17 07:45:35 -0500

    Updated with examples

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-function ajaxCall()
-{
+function ajaxCall() {
   // libreria offerta dal browser
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function ()
-  {
+  xhttp.onreadystatechange = function() {
     // status - return code della richiesta http
     // 200 - richiesta ricevuta con successo
-    if(this.readyState == 4 && this.status == 200)
-    {
+    if (this.readyState == 4 && this.status == 200) {
       // pagina html inviata in risposta dal server
-      document.getElementById("canvas").innerHTML = this.responseText;
+      document.getElementById("canvas").innerHTML = JSON.parse(this.responseText)[1].nome;
     }
   };
-  xhttp.open("GET", "service.php");
+  xhttp.open("GET", "service.php"); //HEI HEI HEI 
   xhttp.send();
 }
--- service.php
+++ service.php
@@ -1,5 +1,28 @@
 <?php
 
-echo "<font size=48>BUONASERAAAA</font>";
+  class Alunno
+  {
+    public $nome, $cognome, $voto;
+    
+    public function __construct($nome, $cognome, $voto)
+    {
+      $this->nome = $nome;
+      $this->cognome = $cognome;
+      $this->voto = $voto;
+    }
+  }
+  
+  header('Content-type: application/json');  //manipola direttamente un pezzo della risposta http
+  //Oggetto Php  
+  $alunno = new Alunno("Marco", "Maimonte", "9.5");
+  $alunno2 = new Alunno("Geordi", "Parriglio", "9.5");
+  //$array = array("nome"=>$alunno->nome,"cognome"=>"Garibaldino");
+  //Trasformazione in php
+  $json = json_encode(array($alunno, $alunno2));
+  
+  //Restituire al client
+  echo $json;
+ 
+ // json_encode();
 
 ?>
